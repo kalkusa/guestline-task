ae3474de9f9c6e57694133d82ec47b8301b2f4a8
Arkadiusz Kałkus <kalkus@10g.pl>
2022-10-29 01:28:59 +0200

    Hotel box layout

--- src/Components/SearchResults/SearchResultItem/SearchResultItem.tsx
+++ src/Components/SearchResults/SearchResultItem/SearchResultItem.tsx
@@ -1,5 +1,5 @@
 import { StarIcon } from "@chakra-ui/icons";
-import { Badge, Box, Image } from "@chakra-ui/react";
+import { AspectRatio, Box, Flex, Image, Spacer } from "@chakra-ui/react";
 import { Hotel } from "../../../Types/Hotel";
 
 interface SearchResultItemProps {
@@ -8,45 +8,27 @@ interface SearchResultItemProps {
 
 export const SearchResultItem = ({ hotel }: SearchResultItemProps) => {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Image src={hotel.images[0].url} alt="hotel image" />
-
-      <Box p="6">
-        {/* <Box display="flex" alignItems="baseline">
-          <Box
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            textTransform="uppercase"
-            ml="2"
-          >
-            {property.beds} beds &bull; {property.baths} baths
-          </Box>
-        </Box> */}
-
-        <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" noOfLines={1}>
-          {hotel.name}
+    <Box w="3xl" borderWidth="1px" borderRadius="lg" p={5} id="searchResultItem">
+      <Flex direction="row" justifyContent="flex-end">
+        <AspectRatio minW="200px" maxW="500px" ratio={4 / 3} mr="5">
+          <Image src={hotel.images[0].url} alt="hotel image" objectFit="cover" />
+        </AspectRatio>
+        <Box id="nameAndAddress" textAlign="left">
+          <p>{hotel.name}</p>
+          <p>{hotel.address1}</p>
+          <p>{hotel.address2}</p>
         </Box>
-
+        <Spacer />
         <Box>
-          {123}
-          <Box as="span" color="gray.600" fontSize="sm">
-            / wk
+          <Box display="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <StarIcon key={i} color={i < hotel.starRating ? "teal.500" : "gray.300"} />
+              ))}
           </Box>
         </Box>
-
-        <Box display="flex" mt="2" alignItems="center">
-          {Array(5)
-            .fill("")
-            .map((_, i) => (
-              <StarIcon key={i} color={i < hotel.starRating ? "teal.500" : "gray.300"} />
-            ))}
-          {/* <Box as="span" ml="2" color="gray.600" fontSize="sm">
-            {property.reviewCount} reviews
-          </Box> */}
-        </Box>
-      </Box>
+      </Flex>
     </Box>
   );
 };
